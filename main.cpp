251838836dedd4db3990bd7890161578bf52fa5b
Soloprogramer <48121360+Soloprogramer@users.noreply.github.com>
2020-09-19 00:32:25 +0430

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,4 @@
-/*
-ID: samanfa1
-TASK: milk2
-LANG: C++14
-*/
+
 
 #ifndef ONLINE_JUDGE
     #include "bits/stdc++.h"
@@ -17,26 +13,17 @@ LANG: C++14
 
 using namespace std;
 
-//#warning not there YET
-//#error hello
-#define _debug freopen("kn.txt","w",stdout);
-#define lint long long int
+
+
 #define endl '\n'
-#define kill(x) return cout<<x<<'\n', 0
-#define debug(x) cout << #x << " : " << x << '\n'
 #define pb push_back
-#define ppb pop_back
-#define speed  cout<<clock()*1000/CLOCKS_PER_SEC<<" ms";
 #define F first
 #define S second
-#define _ri [i<<1|1]
-#define _le [i<<1]
-#define _fast ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr);
+
 typedef long long  ll;
 typedef long double ld;
 
-//int constexpr maxn = 1'000'023;
-//int a[maxn]; // 1-> l( , 2 -> r) , 3-> l,r ()
+
 map<int, pair<int, int> > a; // to anddis i chand ta baze chand ta baste
 vector<pair<int, int> >seg;
 int main() {
@@ -66,22 +53,6 @@ int main() {
             seg.pb(make_pair(begin,end));
             l = 0, r = 0; t++;
         }
-
-        /*
-        if(a[i] == 1) {
-            if(l == 0)
-                begin = i;
-            l++;
-        }
-        else if(a[i] == 2) {
-            r++;
-        }
-        if(l > 0 and l == r){
-            end = i;
-            seg[t].F = begin, seg[t].S = end;
-            l = 0, r = 0, t++;
-        }
-         */
     }
     ll mini = 0;
     for(int i = 0;i < t ;i++)
